2eb88cd76ff90cda8ed30f7c5348e86e8d951e7e
Juneus Magsakay <jbm9397@gmail.com>
2021-06-07 16:46:48 -0400

    initial commit

---
+++ Pythagorean.java
@@ -0,0 +1,7 @@
+public class Pythagorean{
+    public double calculateHypotenuse(double legA, double legB){
+        double pyth = Math.sqrt((legA * legA) + (legB * legB));
+        return pyth;
+    }
+}
+
---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+    public static void main(String[] args){
+    Pythagorean x = new Pythagorean();
+    System.out.println(x.calculateHypotenuse(8.0, 8.0));
+}
+
+}
